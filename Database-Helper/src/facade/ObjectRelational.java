73c968211de899568a3c34f07a5d1b3e1c50acdd
MuriloACQ <macquadros@gmail.com>
2014-08-29 16:47:09 -0300

    javadoc

--- Database-Helper/src/facade/ObjectRelational.java
+++ Database-Helper/src/facade/ObjectRelational.java
@@ -38,18 +38,32 @@ public class ObjectRelational {
 		initialize(resultSet);
 	}
 	
+	/**
+	 * Set a prefix in each column of table
+	 * @param prefix
+	 */
 	public void setPrefix(String prefix) {
 		this.prefix = prefix;
 	}
 	
 	/**
-	 * Database format to Java Object format
+	 * From Database format to Java Object format
 	 * @param mod values {SNAKELOWERCASE_TO_CAMELCASE, SNAKEUPPERCASE_TO_CAMELCASE, UPPERCASE_TO_LOWERCASE, NONE}
 	 */
 	public void setCaseMod(int mod) {
 		selectedCase = mod;
 	}
 	
+	/**
+	 * This method is automatically called when you use a factory or the constructor <ObjectRelational(ResultSet resultSet)>
+	 * @param resultSet
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws SQLException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws InvocationTargetException
+	 */
 	public void initialize(ResultSet resultSet) throws IllegalArgumentException, IllegalAccessException, SQLException, NoSuchMethodException, SecurityException, InvocationTargetException {
 		for (Field field : fields){
 			String columnName = getColumnName(field.getName());
@@ -57,6 +71,15 @@ public class ObjectRelational {
 		}
 	}
 	
+	/**
+	 * Export to format <String key, String value> compatible with table columns 
+	 * @return Map<String, String>
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws InvocationTargetException
+	 */
 	public Map<String, String> export() throws IllegalArgumentException, IllegalAccessException, NoSuchMethodException, SecurityException, InvocationTargetException {
 		Map<String, String> map = new HashMap<String, String>();
 		for(Field field : fields) {
@@ -66,16 +89,38 @@ public class ObjectRelational {
 		return map;
 	}
 	
+	/**
+	 * Get the column name compatible with table
+	 * @param fieldName
+	 * @return column name
+	 */
 	public String getColumnName(String fieldName) {
 		String columnName = setSelectedPrefix(fieldName);
 		columnName = getStringInSelectedCase(columnName);
 		return columnName;
 	}
 	
+	/**
+	 * Get a field declared in the class by name
+	 * @param fieldName
+	 * @return field
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 */
 	public Field getField (String fieldName) throws NoSuchFieldException, SecurityException {
 		return this.getClass().getDeclaredField(fieldName);
 	}
 	
+	/**
+	 * Set a value in a field
+	 * @param field
+	 * @param value
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws InvocationTargetException
+	 */
 	public void setFieldValue(Field field, Object value) throws IllegalArgumentException, IllegalAccessException, NoSuchMethodException, SecurityException, InvocationTargetException {
 		if (field.getModifiers() == 1) {
 			field.set(this, value);
@@ -85,6 +130,16 @@ public class ObjectRelational {
 		}
 	}
 	
+	/**
+	 * Get a field value. Null value will return a string "null"
+	 * @param field
+	 * @return String
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws InvocationTargetException
+	 */
 	public String getFieldValueAsString(Field field) throws IllegalArgumentException, IllegalAccessException, NoSuchMethodException, SecurityException, InvocationTargetException {
 		String result;
 		if (field.getModifiers() == 1) {
@@ -96,18 +151,38 @@ public class ObjectRelational {
 		return result;
 	}
 	
+	/**
+	 * Convert a field name in a set method name using Java standard code definition
+	 * @param field
+	 * @return set method name
+	 */
 	private String convertToSetMethod (Field field) {
 		return "set" + firstLetterToUpperCase(field.getName());
 	}
 	
+	/**
+	 * Convert a field name in a get method name using Java standard code definition
+	 * @param field
+	 * @return get method name
+	 */
 	private String convertToGetMethod (Field field) {
 		return "get" + firstLetterToUpperCase(field.getName());
 	}
 	
+	/**
+	 * Convert a camelcase string in a snakecase string
+	 * @param camelcase
+	 * @return snakecase
+	 */
 	private String camelToSnakeCase(String camelcase) {
 		return camelcase.replaceAll("([A-Z][a-z])", "_$1");
 	}
 	
+	/**
+	 * Convert a string to be compatible with database columns
+	 * @param string
+	 * @return string in the selected format
+	 */
 	private String getStringInSelectedCase(String string) {
 		String result;
 		switch(selectedCase){
@@ -126,6 +201,11 @@ public class ObjectRelational {
 		return result;
 	}
 	
+	/**
+	 * Apply prefix in a column name
+	 * @param columnName
+	 * @return column name with prefix
+	 */
 	private String setSelectedPrefix(String columnName) {
 		if(prefix != null && !prefix.isEmpty()){
 			columnName = prefix.concat(columnName);
@@ -133,6 +213,11 @@ public class ObjectRelational {
 		return columnName;
 	}
 	
+	/**
+	 * Make the first letter uppercase
+	 * @param string
+	 * @return string with first letter uppercase 
+	 */
 	private String firstLetterToUpperCase (String string) {
 		return string.substring(0,1).toUpperCase() + string.substring(1); 
 	}
